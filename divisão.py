b5ec50799ffc6d14915fba4de4e0a0c6c3fa2f03
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-18 11:39:55 -0300

    Add files via upload

---
+++ divisão.py
@@ -0,0 +1,31 @@
+#Escreva um programa que leia dois valores, um dividendo e um divisor. Mostre o resultado da divisão e o resto da divisão inteira dos valores.
+
+dividendo = int(input('Digite o dividendo: '))
+divisor = int(input('Digite o divisor: '))
+quociente = dividendo // divisor
+resto = dividendo % divisor
+FIM = str('FIM')
+print(f'{dividendo} dividido por {divisor}')
+print(f' é igual a {quociente} e o resto')
+
+      
+
+
+
+
+
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+                                     
+                                     
+                        
