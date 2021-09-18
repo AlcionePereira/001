9a795380726ec2d3ad2e43b72bdc69dcdc74bc7e
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-18 10:37:54 -0300

    Delete calculadesconto.py

--- calculadesconto.py
+++
@@ -1,7 +0,0 @@
-preço = float(input("Digite o preço"))
-preço_com_desconto = preço - (preço * 10 / 100)
-preço_com_desconto = round(preço_com_desconto, 2)
-print("Preço com desconto:", preço_com_desconto)
-
-
-
