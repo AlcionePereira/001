4ccc4d26bc7cd4bdf44fe9a19c9946c5c47d2a9a
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-19 17:56:39 -0300

    Add files via upload

---
+++ raioooo.py
@@ -0,0 +1,29 @@
+raio = float(input().strip())
+circunferência = 2 * 3.14 * raio
+print("Circunferência {:.6f} ".format (circunferência))
+a_circulo = 3.14 * raio * 2
+a_esfera = 4 * 3.14 * raio ** 2
+vol_esfera = 4 / 3 * 3.14 * raio ** 3
+print(Área do círculo {:.6f} .format ( a_circulo))
+print(Área da esfera {:.6f} .format ( a_esfera))
+print(Volume da esfera {:.6f} .format ( vol_esfera))
+
+
+
+
+a_circulo = 3.14 * raio * 2
+a_esfera = 4 * 3.14 * raio ** 2
+vol_esfera = 4 / 3 * 3.14 * raio ** 3
+print(a_círculo)
+print(a_esfera)
+print(vol_esfe
+
+circunferencia = 2 * 3.14 * ra
+
+
+
+
+
+
+
+      
